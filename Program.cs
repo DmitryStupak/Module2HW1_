1a23e2515bb27d2832992e355eabc008f828fd07
DmitryStupak <dimast_ukr@outlook.com>
2022-01-21 17:35:18 +0200

    Add StyleCop

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 
 namespace NixFirst4HW
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             int n = 0;
             bool rightNum = false;

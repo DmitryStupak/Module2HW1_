49648347809b717bd15b43e86b23af5201c11e60
DmitryStupak <dimast_ukr@outlook.com>
2022-01-30 18:30:52 +0200

    Add Enum

--- LogType.cs
+++ LogType.cs
@@ -4,19 +4,26 @@ namespace Module2HW1
 {
     public class LogType
     {
-        public LogType(string type, string str)
+        public LogType(Log type, string mes)
         {
             Time = DateTime.Now;
-            Str = str;
+            Mes = mes;
             Type = type;
         }
 
+        public enum Log
+        {
+            Info,
+            Warning,
+            Error
+        }
+
         public DateTime Time { get; set; }
-        public string Str { get; set; }
-        public string Type { get; set; }
+        public string Mes { get; set; }
+        public Log Type { get; set; }
         public override string ToString()
         {
-            return $"{Time} : {Type} : {Str}";
+            return $"{Time} : {Type} : {Mes}";
         }
     }
 }
